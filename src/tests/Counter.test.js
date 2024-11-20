df535d25e775c4580baae50121c0222e5f5479a1
Toni Molina <60618426+Tony-coder95@users.noreply.github.com>
2024-11-20 12:36:16 -0500

    Update Counter.test.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,28 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
-})
+  render(<Counter />);
+});
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(/Counter/i)).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  expect(screen.getByTestId('count').textContent).toBe('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const incrementButton = screen.getByText('+');
+  fireEvent.click(incrementButton);
+  expect(screen.getByTestId('count').textContent).toBe('1');
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const decrementButton = screen.getByText('-');
+  fireEvent.click(decrementButton);
+  expect(screen.getByTestId('count').textContent).toBe('-1');
 });
+
