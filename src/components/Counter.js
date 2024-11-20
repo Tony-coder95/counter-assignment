bd351bbc818354cbdf8e2fe7fc1626a59528550b
Toni Molina <60618426+Tony-coder95@users.noreply.github.com>
2024-11-20 12:46:58 -0500

    Update Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,17 +4,19 @@ export default function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount((count) => count + 1);
+    setCount((prevCount) => prevCount + 1);
   }
 
   function decrement() {
-    setCount((count) => count - 1);
+    setCount((prevCount) => prevCount - 1);
   }
 
   return (
     <div>
       <h2>Counter</h2>
+      {/* Display the current count */}
       <p data-testid="count">{count}</p>
+      {/* Buttons to increment and decrement */}
       <button onClick={increment}> + </button>
       <button onClick={decrement}> - </button>
     </div>
